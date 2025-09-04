3a790fd1318059f882641100d626f0ca14cd9be7
Mohammad Sharzehei <sharzehm@gmail.com>
2025-09-04 17:07:26 +0100

    Refactor Food class in food.py

--- food.py
+++ food.py
@@ -14,37 +14,7 @@ class Food(Turtle):
         self.p_food = 0
         self.refresh()
         self.color('blue')
-        # self.eat = False
 
     def refresh(self):
-        # food_snake = Turtle('circle')
-        # food_snake.shapesize(0.5, 0.5)
-        # food_snake.speed(6)
-        # food_snake.penup()
         self.p_food = (random.randint(-280, 280), random.randint(-280, 250))
-        # position_of_snake = []
-        # for segment_of_snake in segments_of_snake:
-        #     position_of_snake.append(segment_of_snake.position())
-        # while self.p_food in position_of_snake:
-        #     self.p_food = (random.randint(-280, 280), random.randint(-280, 280))
         self.goto(self.p_food)
-
-    # def eat_food(self, segments_of_snake):
-    #     if segments_of_snake[0].distance(self.p_food) < 15 :
-    #         self.refresh(segments_of_snake)
-    #         self.eat = True
-    #         print("yessss")
-    #         # insert_snake()
-    #         # creat_food()
-
-    # def insert_snake():
-    #     new_snake = Turtle('square')
-    #     new_snake.speed(6)
-    #     new_snake.penup()
-    #     last_part_snake = []
-    #     last_part_snake.append(all_part_of_snake[len(all_part_of_snake) - 1].position())
-    #     move_snake()
-    #     new_snake.goto(last_part_snake[0])
-    #     new_snake.color('white')
-    #     all_part_of_snake.append(new_snake)
-
